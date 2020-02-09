d540946a5e7eb32ba62c46f2a71a3decd767f97f
geisterfurz007 <michelvonv@me.com>
2020-02-09 15:47:45 +0100

    Reformat

--- lib/services/life_cycle_listener.dart
+++ lib/services/life_cycle_listener.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:random_alarm/services/alarm_scheduler.dart';
 import 'package:random_alarm/services/file_proxy.dart';
 import 'package:random_alarm/stores/alarm_list/alarm_list.dart';
 
 class LifeCycleListener extends WidgetsBindingObserver {
-
   final AlarmList alarms;
 
   LifeCycleListener(this.alarms);
 
   @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
     switch (state) {
       case AppLifecycleState.paused:
-        alarms.alarms.forEach((alarm) => alarm.updateMusicPaths());
-        JsonFileStorage().writeList(alarms.alarms);
+        AlarmScheduler().testAlarm();
+        saveAlarms();
         break;
       default:
     }
   }
 
+  void saveAlarms() {
+    alarms.alarms.forEach((alarm) => alarm.updateMusicPaths());
+    JsonFileStorage().writeList(alarms.alarms);
+  }
 }
