9898b13b0946a51d90048040ca8dd1484ee567e9
geisterfurz007 <michelvonv@me.com>
2020-02-11 15:52:18 +0100

    Make note about required change

--- lib/services/alarm_scheduler.dart
+++ lib/services/alarm_scheduler.dart
@@ -5,6 +5,11 @@ class AlarmScheduler {
     print('Initialize AlarmService');
     await AndroidAlarmManager.initialize().then(print);
     print('Setting oneShot');
+    /*
+      To wake up the device and run something on top of the lockscreen,
+      this currently requires the hack from here to be implemented:
+      https://github.com/flutter/flutter/issues/30555#issuecomment-501597824
+    */
     await AndroidAlarmManager.oneShot(
             Duration(seconds: 10), 42, callback, wakeup: true)
         .then(print);
