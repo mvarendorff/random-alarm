5c1c09a95a9029608379eaaf8f4123d314bbc394
geisterfurz007 <michelvonv@me.com>
2020-02-11 15:32:16 +0100

    Updates to the android bits to allow displaying the app in lockscreen

--- android/app/src/main/kotlin/io/github/geisterfurz007/playground/MainActivity.kt
+++ android/app/src/main/kotlin/io/github/geisterfurz007/playground/MainActivity.kt
@@ -1,11 +1,31 @@
 package io.github.geisterfurz007.playground
 
 import androidx.annotation.NonNull;
+import android.os.Bundle;
+import android.os.Build;
+import android.content.Context;
+import android.app.KeyguardManager;
+import android.view.WindowManager;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true)
+            setTurnScreenOn(true)
+            val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+            keyguardManager.requestDismissKeyguard(this, null)
+        } else {
+            this.window.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
+                    WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                    WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON)
+        }
+    }
+
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
