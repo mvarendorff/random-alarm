dee13784222c6ac621b309443f267dfcf6251e80
geisterfurz007 <michelvonv@me.com>
2020-02-24 19:20:40 +0100

    Updated main dart to initialize the AlarmManager

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:android_alarm_manager/android_alarm_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:random_alarm/screens/home_screen/home_screen.dart';
-import 'package:random_alarm/services/alarm_scheduler.dart';
 import 'package:random_alarm/services/file_proxy.dart';
 import 'package:random_alarm/services/life_cycle_listener.dart';
 import 'package:random_alarm/stores/alarm_list/alarm_list.dart';
@@ -12,10 +12,10 @@ void main() async {
   final alarms = await new JsonFileStorage().readList();
   list.setAlarms(alarms);
   list.alarms.forEach((alarm) => alarm.loadTracks());
-
   WidgetsBinding.instance.addObserver(LifeCycleListener(list));
 
   runApp(MyApp());
+  await AndroidAlarmManager.initialize();
 }
 
 class MyApp extends StatelessWidget {
