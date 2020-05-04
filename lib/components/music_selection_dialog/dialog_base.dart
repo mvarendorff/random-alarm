67a4f45f3b12b4ebd6bde2dbcd4b99817000f51d
geisterfurz007 <michelvonv@me.com>
2020-05-04 12:12:44 +0200

    chore: refactored music_selection_dialog
    To make it easier to introduce other searchable dialogs in the future,
    this commit creates a new DialogBase and makes use of that in the
    music_selection_dialog

---
+++ lib/components/music_selection_dialog/dialog_base.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_audio_query/flutter_audio_query.dart';
+import 'package:mobx/mobx.dart';
+import 'package:random_alarm/stores/music_selection/music_selection.dart';
+import 'package:random_alarm/stores/observable_alarm/observable_alarm.dart';
+
+import 'music_selection_dialog.dart';
+
+/// Base class for a searchable dialog
+///
+/// Not sure I really want this tbh O-O
+class DialogBase extends StatelessWidget {
+  // -- Base
+
+  final Function onDone;
+  final Function onSearchChange;
+  final Function onSearchClear;
+  final Widget child;
+
+
+  DialogBase({Key key, this.onDone, this.child, this.onSearchChange, this.onSearchClear})
+        : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = TextEditingController();
+
+    return Dialog(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(child: TextField(onChanged: onSearchChange, controller: controller)),
+                IconButton(
+                  icon: Icon(Icons.clear),
+                  onPressed: () {
+                    controller.clear();
+                  },
+                )
+              ],
+            ),
+            Expanded(child: this.child),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                FlatButton(
+                  child: Text('Cancel'),
+                  onPressed: () => Navigator.pop(context),
+                ),
+                FlatButton(
+                  child: Text('Done'),
+                  onPressed: () {
+                    onDone();
+                    return Navigator.pop(context);
+                  },
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+}
--- lib/components/music_selection_dialog/music_selection_dialog.dart
+++ lib/components/music_selection_dialog/music_selection_dialog.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_audio_query/flutter_audio_query.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:mobx/mobx.dart';
+import 'package:random_alarm/components/music_selection_dialog/dialog_base.dart';
 import 'package:random_alarm/stores/music_selection/music_selection.dart';
 import 'package:random_alarm/stores/observable_alarm/observable_alarm.dart';
 
@@ -13,55 +14,25 @@ class MusicSelectionDialog extends StatelessWidget {
 
   MusicSelectionDialog({Key key, this.titles, this.alarm})
       : store = MusicSelectionStore(titles,
-            alarm.trackInfo.map((info) => info.id).toList()),
+      alarm.trackInfo.map((info) => info.id).toList()),
         super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
-    final controller = TextEditingController();
+    final onDone = () {
+      final newSelected = store.trackSelected.entries
+          .where((entry) => entry.value)
+          .map((entry) => entry.key);
+      alarm.musicIds = ObservableList.of(newSelected);
+      alarm.loadTracks();
+    };
 
-    return Dialog(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Expanded(child: TextField(onChanged: (newValue) => store.currentSearch = newValue ,controller: controller)),
-                IconButton(
-                  icon: Icon(Icons.clear),
-                  onPressed: () {
-                    controller.clear();
-                    store.clearSearch();
-                  },
-                )
-              ],
-            ),
-            Expanded(child: MusicList(store: store)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                FlatButton(
-                  child: Text('Cancel'),
-                  onPressed: () => Navigator.pop(context),
-                ),
-                FlatButton(
-                  child: Text('Done'),
-                  onPressed: () {
-                    final newSelected = store.trackSelected.entries
-                        .where((entry) => entry.value)
-                        .map((entry) => entry.key);
-                    alarm.musicIds = ObservableList.of(newSelected);
-                    alarm.loadTracks();
-                    return Navigator.pop(context);
-                  },
-                )
-              ],
-            )
-          ],
-        ),
-      ),
+    return DialogBase(
+      onDone: onDone,
+      child: MusicList(store: store),
+      onSearchChange: (newValue) => store.currentSearch = newValue,
+      onSearchClear: () => store.clearSearch(),
     );
   }
 }
@@ -73,12 +44,12 @@ class MusicList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Observer(
-      builder: (context) => ListView(
-        shrinkWrap: true,
-        children: store.filteredIds.map(widgetForSongId).toList(),
-      ),
+      builder: (context) =>
+          ListView(
+            shrinkWrap: true,
+            children: store.filteredIds.map(widgetForSongId).toList(),
+          ),
     );
   }
 
@@ -87,13 +58,14 @@ class MusicList extends StatelessWidget {
     final title = titles.firstWhere((info) => info.id == id);
 
     return Observer(
-        builder: (context) => CheckboxListTile(
-          value: store.trackSelected[title.id] ?? false,
-          title: Text(title.title ?? title.displayName),
-          onChanged: (newValue) {
-            return store.trackSelected[title.id] = newValue;
-          },
-        ));
+        builder: (context) =>
+            CheckboxListTile(
+              value: store.trackSelected[title.id] ?? false,
+              title: Text(title.title ?? title.displayName),
+              onChanged: (newValue) {
+                return store.trackSelected[title.id] = newValue;
+              },
+            ));
   }
 
 }
