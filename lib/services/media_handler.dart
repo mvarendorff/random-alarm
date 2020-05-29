152ddafa3d2d5d29cf0287ddfe70e59480274da9
geisterfurz007 <michelvonv@me.com>
2020-05-29 17:44:10 +0200

    fix: playlist issues / feat: constant sounds (#8)
    There were a few issues in how playlists were handled which are fixed.
    I also got the idea of #8 working (I think) so now short sounds are
    looped, long sounds are considered songs and cause a new random sound of
    the selection to play.

--- lib/services/media_handler.dart
+++ lib/services/media_handler.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'dart:io';
 import 'dart:math';
 
 import 'package:audioplayers/audioplayers.dart';
@@ -16,35 +18,72 @@ class MediaHandler {
     Volume.setVol(newVolume);
   }
 
-  playMusic(ObservableAlarm alarm) async {
+  getRandomPath(ObservableAlarm alarm) async {
     final FlutterAudioQuery query = FlutterAudioQuery();
     final allPlaylists = await query.getPlaylists();
 
-    final playlistSongIds = allPlaylists
+    final playlistSongIdChunks = allPlaylists
         .where((playlist) => alarm.playlistIds.contains(playlist.id))
-        .map((info) => info.memberIds)
-        .reduce((a, b) => [...a, ...b]);
+        .map((info) => info.memberIds);
+
+    var playlistSongIds;
+    if (playlistSongIdChunks.length > 1)
+      playlistSongIds = playlistSongIdChunks.reduce((a, b) => [...a, ...b]);
+    else if (playlistSongIdChunks.length == 1)
+      playlistSongIds = playlistSongIdChunks.toList()[0];
+    else
+      playlistSongIds = [];
 
     // Workaround for the case of a single playlist that has just one song
     // https://github.com/sc4v3ng3r/flutter_audio_query/issues/16
-    final playlistPaths =
-        (await query.getSongsById(ids: [...playlistSongIds, ""]))
-            .map((info) => info.filePath);
+    // Adding hack after hack; good times
+    final Iterable playlistPaths = playlistSongIds.length > 0
+        ? (await query.getSongsById(ids: [...playlistSongIds, ""]))
+            .map((info) => info.filePath)
+        : [];
 
     final paths = [...alarm.musicPaths, ...playlistPaths];
     print('Paths: $paths');
 
     final entry = Random().nextInt(paths.length);
-    final path = paths[entry];
+    return paths[entry];
+  }
 
-    //If empty, get default ringtone
-    //Pick a random path, pass it to the player; for testing just print it
+  playMusicFromPath(String path, alarm) async {
+    bool subscribed = false;
     _currentPlayer = AudioPlayer();
-    _currentPlayer.play(path, isLocal: true, volume: 1.0);
+
+    StreamSubscription subscription;
+    subscription = _currentPlayer.onDurationChanged.listen((duration) {
+      final seconds = duration.inSeconds;
+      if (seconds < 30) {
+        _currentPlayer.setReleaseMode(ReleaseMode.LOOP);
+        subscription.cancel();
+      } else {
+        if (subscribed) return;
+        _currentPlayer.onPlayerCompletion.listen((_) async {
+          playMusic(alarm);
+          subscription.cancel();
+        });
+        subscribed = true;
+      }
+    });
+
+    final fixedPath = File(path).absolute.path;
+    await _currentPlayer.setUrl(fixedPath, isLocal: true);
+    _currentPlayer.play(fixedPath, isLocal: true, volume: 1.0);
+  }
+
+  playMusic(ObservableAlarm alarm) async {
+    final path = await getRandomPath(alarm);
+    playMusicFromPath(path, alarm);
   }
 
   stopAlarm() {
-    _currentPlayer.stop();
     Volume.setVol(_originalVolume);
+    if (_currentPlayer == null) return;
+
+    _currentPlayer.stop();
+    _currentPlayer.release();
   }
 }
